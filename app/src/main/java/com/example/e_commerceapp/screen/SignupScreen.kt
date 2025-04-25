eb109c598b46971f61a93fc482efa3bccb61dea1
Hermi-git <andargiehermela1@gmail.com>
2025-04-25 07:26:26 +0300

    build signup screen

--- app/src/main/java/com/example/e_commerceapp/screen/SignupScreen.kt
+++ app/src/main/java/com/example/e_commerceapp/screen/SignupScreen.kt
@@ -1,11 +1,113 @@
 package com.example.e_commerceapp.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.e_commerceapp.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 @Composable
 fun SignupScreen(modifier: Modifier = Modifier) {
-    Text(text = "Signup page")
+    var email by remember{
+        mutableStateOf("")
+    }
+    var name by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hello there!",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
 
+        Text(
+            text = "Create an account",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 22.sp,
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.loginbanner),
+            contentDescription = "Banner",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value= email,
+            onValueChange = {email = it},
+            label = {
+                Text(text = "Email Address")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value= name,
+            onValueChange = {name = it},
+            label = {
+                Text(text = "Full Name")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value= password,
+            onValueChange = {password= it},
+            label = {
+                Text(text = "Password")
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+                .height(60.dp)
+        ) {
+            Text(text= "SignUp", fontSize = 22.sp)
+        }
+    }
 }
